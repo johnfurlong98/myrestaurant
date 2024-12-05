08edbdb3f99a8562a3f0108c4ad3c0534f2338dc
john furlong <furlongjohn088@gmail.com>
2024-12-05 16:10:49 +0000

    added reservations views.py for admin

--- reservations/views.py
+++ reservations/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# reservations/views.py
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from accounts.decorators import role_required
+from .models import Booking
+
+@login_required
+@role_required('ADMIN')
+def admin_booking_management(request):
+    """
+    Admin view to manage all bookings.
+    """
+    bookings = Booking.objects.all()
+    return render(request, 'reservations/admin_booking_management.html', {'bookings': bookings})
